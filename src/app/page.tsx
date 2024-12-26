ff80230a47d21a78199e5c95e4ac315da512a3f7
ShumailaWaheed <alishanaz155@gmail.com>
2024-12-26 14:17:56 -0800

    Mini figma template assignment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-screen bg-gray-100 flex justify-center items-center">
-      <div className="flex flex-col md:flex-row h-auto lg:h-[465px] w-11/12">
+    <div className="h-screen  bg-gray-100 flex justify-center items-center ">
+      <div className="flex flex-col md:flex-row h-auto lg:h-[465px]  w-11/12">
         <div className="flex flex-col flex-1 gap-4 justify-center px-4 text-center md:text-left">
-          <h1 className="font-bold text-[32px] sm:text-[32px] md:text-[35px] lg:text-[44px]">
+          <h1 className="font-bold text-[32px] text-black md:text-[35px] lg:text-[44px]">
             Welcome To Our Website
           </h1>
-          <p className="font-normal text-[18px] sm:text-[18px] md:text-[22px] lg:text-[26px] xl:text-[32px]">
+          <p className="font-normal text-[20px] text-black md:text-[22px] lg:text-[26px] xl:text-[32px]">
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
             Lorem Ipsum has been the industry standard
           </p>
@@ -18,7 +18,6 @@ export default function Home() {
             </button>
           </div>
         </div>
-
         <div className="flex-1 flex justify-center items-center w-full h-auto mt-8 md:mt-0 md:ml-8">
           <Image 
             src="/images/main.png"
